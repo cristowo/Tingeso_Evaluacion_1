6832bafbe80610932d1a926ba86de297a8e08d36
cri <cristobal.marchant@usach.cl>
2023-03-23 10:49:51 -0300

    2nd Commit 😎👍

---
+++ Tingeso1/src/main/java/MilkStgo/Tingeso1/Tingeso1Application.java
@@ -0,0 +1,13 @@
+package MilkStgo.Tingeso1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tingeso1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tingeso1Application.class, args);
+	}
+
+}
---
+++ Tingeso1/src/test/java/MilkStgo/Tingeso1/Tingeso1ApplicationTests.java
@@ -0,0 +1,13 @@
+package MilkStgo.Tingeso1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tingeso1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
