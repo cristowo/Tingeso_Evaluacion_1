cd5ddbb80ec77da797c6c2b59a78443312dcd64e
cri <cristobal.marchant@usach.cl>
2023-04-04 16:07:00 -0400

    gracias juan

--- Tingeso1/src/main/java/MilkStgo/Tingeso1/entities/ProveedorEntity.java
+++ Tingeso1/src/main/java/MilkStgo/Tingeso1/entities/ProveedorEntity.java
@@ -3,6 +3,7 @@ package MilkStgo.Tingeso1.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- Tingeso1/src/main/java/MilkStgo/Tingeso1/servicies/ProveedorService.java
+++ Tingeso1/src/main/java/MilkStgo/Tingeso1/servicies/ProveedorService.java
@@ -1,11 +1,31 @@
 package MilkStgo.Tingeso1.servicies;
 
+import MilkStgo.Tingeso1.entities.ProveedorEntity;
 import MilkStgo.Tingeso1.repositories.ProveedorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class ProveedorService {
     @Autowired
     ProveedorRepository proveedorRepository;
+
+    @PostMapping("/cargarProveedor")
+    public String cargarProveedor(@RequestParam("nombre") String nombre,
+                                  @RequestParam("codigo") Integer codigo,
+                                  @RequestParam("categoria") String categoria,
+                                  @RequestParam("retencion") Boolean retencion){
+        return "ola";
+    }
+
+    public void guardarProveedor(String nombre, Integer codigo, String categoria, Boolean retencion){
+        ProveedorEntity nuevo_proveedor = new ProveedorEntity();
+        nuevo_proveedor.setCodigo(codigo);
+        nuevo_proveedor.setNombre(nombre);
+        nuevo_proveedor.setRetencion(retencion);
+        nuevo_proveedor.setCategoria(categoria);
+        proveedorRepository.save(nuevo_proveedor);
+    }
 }
